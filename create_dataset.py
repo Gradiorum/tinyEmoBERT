0741ae12a6fd407b4357699da7032a0d65e5f166
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2023-09-20 20:27:03 -0400

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -21,10 +21,42 @@ Love
 happy (mapped to joy)
 """
 
-def txt_to_csv(file_path):
-    _, file_extension = os.path.splitext(file_path)
-    
-    #Check if file is a TXT file
-    
-    if file_extension == 'txt':
+
+
+def delimiter_detection(file_path):
+
+                
+                
+    delimiter_counts = Counter()
+    try:
+        with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+            for line in file:
+                delimiter_counts.update(line.strip())
+        delimiter_types = ['\t', ';', ',']
+        detected = max([(delimiter, delimiter_counts.get(delimiter, 0)) for delimiter in delimiter_types], key=lambda x: x[1])
+        return detected[0]
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+
+def text_to_csv(root_folder):
+    try:
+        for folder_path , _, filenames in os.walk(root_folder):
+            for filename in os.listdir(folder_path):
+                if filename.endswith(".txt"):
+                    file_path = os.path.join(folder_path, filename)
+                    
+                    delimiter = delimiter_detection(file_path)
+                    
+                    df = pd.read_csv(file_path, delimiter=delimiter)
+                    
+                    csv_file_path = os.path.join(folder_path, filename.replace('.txt', '.csv'))
+                    df.to_csv(csv_file_path, index=False)
+                else:
+                    print(f"The given file is not a txt file, cancelling conversion")
+                    continue
+        
+    except FileNotFoundError:
+        print(f"Directory {file_path} not found. ")
+        
+
         
