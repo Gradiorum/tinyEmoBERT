0f7703c1de7d8b6ff85b884b1d4688c4681c43ad
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2023-10-09 13:47:20 -0400

    Add files via upload

--- create_dataset.py
+++ create_dataset.py
@@ -2,6 +2,11 @@ import os
 import pandas as pd
 import csv
 from collections import Counter
+import json
+import gzip
+
+
+
 """
 For the dataset collection method we personally will vet the dataset, even if there are some
 categories in datasets that are not present in other datasets, we still would be able to add it to the dataset
@@ -38,25 +43,53 @@ def delimiter_detection(file_path):
     except FileNotFoundError:
         print(f"File {file_path} not found.")
 
-def text_to_csv(root_folder):
+def convert_files_to_csv(root_folder):
     try:
         for folder_path , _, filenames in os.walk(root_folder):
-            for filename in os.listdir(folder_path):
-                if filename.endswith(".txt"):
-                    file_path = os.path.join(folder_path, filename)
-                    
-                    delimiter = delimiter_detection(file_path)
-                    
-                    df = pd.read_csv(file_path, delimiter=delimiter)
-                    
-                    csv_file_path = os.path.join(folder_path, filename.replace('.txt', '.csv'))
+            for filename in filenames:
+                file_path = os.path.join(folder_path, filename)
+                
+                # Check if the file is a .json.gz
+                if filename.endswith('.json.gz'):
+                    with gzip.GzipFile(file_path, 'r') as f:
+                        json_data = json.loads(f.read().decode('utf-8'))
+                    df = pd.DataFrame(json_data)
+                    csv_file_path = os.path.join(folder_path, filename.replace('.json.gz', '.csv'))
                     df.to_csv(csv_file_path, index=False)
+                
+                # Check if the file is a .txt
+                elif filename.endswith('.txt'):
+                    delimiter = delimiter_detection(file_path)
+                    if delimiter is not None:
+                        df = pd.read_csv(file_path, delimiter=delimiter)
+                        csv_file_path = os.path.join(folder_path, filename.replace('.txt', '.csv'))
+                        df.to_csv(csv_file_path, index=False)
+                
                 else:
-                    print(f"The given file is not a txt file, cancelling conversion")
+                    print(f"The given file {filename} is not supported for conversion.")
                     continue
-        
-    except FileNotFoundError:
-        print(f"Directory {file_path} not found. ")
-        
 
+    except FileNotFoundError:
+        print(f"Directory {root_folder} not found.")
         
+        
+def normalize_dataset(root_folder):
+    for folder_path, _, filenames in os.walk(root_folder):
+        for filename in filenames:
+            if filename.endswith('.csv'):
+                file_path = os.path.join(folder_path,filename)
+                df = pd.read_csv(file_path)
+
+
+def process_dataset(root_folder, mode='unbalanced'):
+    
+    all_data = []
+    
+    if mode == 'balanced':
+        holder = holder
+    elif mode == 'unbalanced':
+        holder = holder
+    else:
+        raise ValueError("Invalid mode. Choose between 'balanced and 'unbalanced")
+    
+    
